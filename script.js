ad386ce4d4ba2123063493d7a68e6a31ef016a8d
= <=>
2022-03-03 18:00:06 -0300

    add loader

--- script.js
+++ script.js
@@ -5,40 +5,51 @@ buttonSubmit.addEventListener("click", async (e) => {
 
   const unFollowersDiv = document.getElementById("unfollowers");
   const username = document.getElementById("username").value;
+  const spinner = document.getElementById("spinner");
 
   if (username === "") {
     alert("Por favor, digite o nome de usuário.");
     return;
   }
 
-  const followers = await fetch(
-    `https://api.github.com/users/${username}/followers`
-  ).then((data) => data.json());
+  unFollowersDiv.innerHTML = "";
+  spinner.classList.toggle("spinner-border");
 
-  const following = await fetch(
-    `https://api.github.com/users/${username}/following`
-  ).then((data) => data.json());
+  try {
+    const followers = await fetch(
+      `https://api.github.com/users/${username}/followers`
+    ).then((data) => data.json());
 
-  const unFollowers = following.filter((user) => {
-    const isFollowingBack = followers.find(
-      (follower) => follower.login === user.login
-    );
+    const following = await fetch(
+      `https://api.github.com/users/${username}/following`
+    ).then((data) => data.json());
 
-    if (!isFollowingBack) {
-      return user;
-    }
-  });
+    const unFollowers = following.filter((user) => {
+      const isFollowingBack = followers.find(
+        (follower) => follower.login === user.login
+      );
 
-  unFollowersDiv.innerHTML = `<h2 class="my-3">Lista de usuários (${unFollowers.length})</h2>`;
-  unFollowers.forEach((unFollower) => {
-    unFollowersDiv.innerHTML += `
-          <div class="card" style="width: 14rem; margin: 0 auto 2rem;">
-            <img src="${unFollower.avatar_url}" class="card-img-top" alt="${unFollower.login}">
-            <div class="card-body">
-              <h5 class="card-title">${unFollower.login}</h5>
-              <a href="${unFollower.html_url}" target="_blank" class="btn btn-primary">Ver perfil</a>
+      if (!isFollowingBack) {
+        return user;
+      }
+    });
+
+    unFollowersDiv.innerHTML = `<h2 class="my-3">Lista de usuários (${unFollowers.length})</h2>`;
+    unFollowers.forEach((unFollower) => {
+      unFollowersDiv.innerHTML += `
+      <div class="card" style="width: 14rem; margin: 0 auto 2rem;">
+              <img src="${unFollower.avatar_url}" class="card-img-top" alt="${unFollower.login}">
+              <div class="card-body">
+                <h5 class="card-title">${unFollower.login}</h5>
+                <a href="${unFollower.html_url}" target="_blank" class="btn btn-primary">Ver perfil</a>
+                </div>
             </div>
-          </div>
-          `;
-  });
+            `;
+    });
+  } catch (error) {
+    console.log(error);
+    alert("Ops, algo deu errado! verifique se o nome de usuário está correto.");
+  }
+
+  spinner.classList.toggle("spinner-border");
 });
